91ef7323821f0a8b743b2bf58f0d33b849d0587c
sha-math <salmarag@kent.edu>
2020-07-24 11:29:18 -0400

    I added another line. Thats it!

--- tritrophic_data_cleaning.R
+++ tritrophic_data_cleaning.R
@@ -627,4 +627,7 @@ write.csv(sbc.napl.mob.invt, file="cleaned_data/SBC_consumer_minvert_napl.csv", 
 write.csv(sbc.napl.ses.invt, file="cleaned_data/SBC_consumer_sinvert_napl.csv", row.names=FALSE)
 write.csv(sbc.napl.algae, file="cleaned_data/SBC_producer_algae_napl.csv", row.names=FALSE)
 
+#We have a meeting at 12pm :D
+#here we go again :P
+
 #hi sha! we are connected via github now!
